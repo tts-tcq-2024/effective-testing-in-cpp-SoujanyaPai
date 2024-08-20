23d1040a3d0c96656dec4e0ceffe45c5b180cc93
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-21 01:57:46 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,8 +17,8 @@ void testTshirtSizes(){
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(38) == "M");
-    assert(size(38) == "S");
+    assert(size(38) == 'M');
+    assert(size(38) == 'S');
     assert(size(42) == 'M');
     assert(size(42) == 'L');
 
